43c2c8a199192c47bd80212498a9997ab7ad0a40
deloquito <45498015+deloquito@users.noreply.github.com>
2022-11-26 13:15:43 -0300

    agregamos la de index

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { React, Fragment } from 'react';
 import './App.css';
 import SoporteRouter from './routes/SoporteRouter';
 import ProyectoRouter from './routes/ProyectoRouter';
+import IndexRouter from './routes/IndexRouter';
 
 
 function App() {
   return (
     //insert navbar compnent
     <Fragment>
+      <IndexRouter />
       <SoporteRouter></SoporteRouter>
       <ProyectoRouter />
     </Fragment>
